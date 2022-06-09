fa63de585dc580a20bdf43fcd7e5c9a9e4594651
Jérôme BON <jerome@hellowatt.fr>
2022-06-09 23:39:02 +0200

    Fix release

--- django_compose_tag/__init__.py
+++ django_compose_tag/__init__.py
@@ -1,7 +1,7 @@
 import django
 
 __title__ = "Django Compose Tag"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Jérôme Bon"
 __license__ = "BSD 3-Clause"
 __copyright__ = "Copyright 2022 Hello Watt"
